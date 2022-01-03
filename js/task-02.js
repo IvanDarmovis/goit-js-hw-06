0db989826c173ac83c607906b05bde398abb785e
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-03 16:30:43 +0200

    second task solved

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const tagList = ingredients.map((ingredient) => {
+  const tag = document.createElement("li");
+  tag.textContent = ingredient;
+  tag.classList.add("item");
+  return tag;
+});
+
+listEl.append(...tagList);
