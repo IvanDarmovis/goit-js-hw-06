b870031ef58c273b36a3c0e259441e7fed699d64
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-03 19:06:05 +0200

    eighth task solved

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть всі поля!");
+  } else {
+    return console.log({
+      email: email.value,
+      password: password.value,
+    });
+  }
+}
