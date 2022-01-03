94e87bb02db93e84c7f49b85794aea1077936ead
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-03 19:13:23 +0200

    ninth task solved

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBtn = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+
+colorBtn.addEventListener("click", onColorBtnClick);
+
+function onColorBtnClick() {
+  const color = getRandomHexColor();
+
+  textEl.textContent = color;
+  document.body.style.backgroundColor = color;
+}
